c126f65b39f76484ff1a605f2842de17d8aa9da6
Tom <Tom@TOMS-DESKTOP>
2017-01-16 22:09:06 -0500

    Added a multi-threaded example.

--- HTTPHelper Example/Form1.designer.vb
+++ HTTPHelper Example/Form1.designer.vb
@@ -34,6 +34,7 @@ Partial Class Form1
         Me.OpenFileDialog = New System.Windows.Forms.OpenFileDialog()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnDownloadFile2 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'btnGetWebPageData
@@ -126,7 +127,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(558, 41)
+        Me.Button1.Location = New System.Drawing.Point(558, 79)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(222, 23)
         Me.Button1.TabIndex = 9
@@ -142,11 +143,21 @@ Partial Class Form1
         Me.Label2.TabIndex = 10
         Me.Label2.Text = "Label2"
         '
+        'btnDownloadFile2
+        '
+        Me.btnDownloadFile2.Location = New System.Drawing.Point(558, 41)
+        Me.btnDownloadFile2.Name = "btnDownloadFile2"
+        Me.btnDownloadFile2.Size = New System.Drawing.Size(222, 23)
+        Me.btnDownloadFile2.TabIndex = 11
+        Me.btnDownloadFile2.Text = "Multi-Threaded Download File"
+        Me.btnDownloadFile2.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(828, 581)
+        Me.Controls.Add(Me.btnDownloadFile2)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.btnStopDownload)
@@ -177,4 +188,5 @@ Partial Class Form1
     Friend WithEvents OpenFileDialog As OpenFileDialog
     Friend WithEvents Button1 As Button
     Friend WithEvents Label2 As Label
+    Friend WithEvents btnDownloadFile2 As Button
 End Class
--- HTTPHelper Example/Form1.vb
+++ HTTPHelper Example/Form1.vb
@@ -5,14 +5,14 @@ Public Class Form1
         Try
             Dim strServerResponse As String = Nothing
 
-            Dim httpHelper As New Tom.httpHelper()
+            Dim httpHelper As New HTTPHelper.httpHelper()
             httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
             httpHelper.addGETData("test3", "value3")
             httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/")
             httpHelper.addHTTPHeader("myheader", "my header contents")
             httpHelper.setHTTPCredentials("test", "test")
 
-            httpHelper.setCustomErrorHandler = Function(ex As Exception, classInstance As Tom.httpHelper)
+            httpHelper.setCustomErrorHandler = Function(ex As Exception, classInstance As HTTPHelper.httpHelper)
                                                    MsgBox(ex.Message)
                                                    Return False
                                                End Function
@@ -26,7 +26,7 @@ Public Class Form1
                     TextBox1.Text &= certDetails.ToString
                 End If
             End If
-        Catch ex As Tom.httpProtocolException
+        Catch ex As HTTPHelper.httpProtocolException
             ' You can handle httpProtocolExceptions different than normal exceptions with this code.
         Catch ex As Net.WebException
             ' You can handle web exceptions different than normal exceptions with this code.
@@ -39,7 +39,7 @@ Public Class Form1
         Try
             Dim strServerResponse As String = Nothing
 
-            Dim httpHelper As New Tom.httpHelper()
+            Dim httpHelper As New HTTPHelper.httpHelper()
             httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
             httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/")
             httpHelper.addHTTPHeader("myheader", "my header contents")
@@ -82,32 +82,33 @@ Public Class Form1
 
     Private Sub btnStopDownload_Click(sender As Object, e As EventArgs) Handles btnStopDownload.Click
         downloadThread.Abort()
+        If statusThread IsNot Nothing Then statusThread.Abort()
     End Sub
 
-    Private downloadThread As Threading.Thread
+    Private downloadThread, statusThread As Threading.Thread
 
     Private Sub btnDownloadFile_Click(sender As Object, e As EventArgs) Handles btnDownloadFile.Click
         ' First we create our httpHelper Class instance.
-        Dim httpHelper As New Tom.httpHelper()
+        Dim httpHelper As New HTTPHelper.httpHelper()
         httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
 
         ' Now we set up our download status updating Lambda function that's passed to the Class instance to execute within the memory space of the Class.
-        httpHelper.setDownloadStatusUpdateRoutine = Function(ByVal downloadStatusDetails As Tom.downloadStatusDetails)
-                                                        Label1.Text = String.Format("Downloaded {0} of {1}", httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.remoteFileSize))
-                                                        Label2.Text = downloadStatusDetails.percentageDownloaded.ToString & "%"
-                                                        ProgressBar1.Value = downloadStatusDetails.percentageDownloaded
-                                                        Return False
-                                                    End Function
+        httpHelper.setDownloadStatusUpdateRoutine = Sub(ByVal downloadStatusDetails As HTTPHelper.downloadStatusDetails)
+                                                        Label1.Invoke(Sub() Label1.Text = String.Format("Downloaded {0} of {1}", httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.remoteFileSize)))
+                                                        Label2.Invoke(Sub() Label2.Text = downloadStatusDetails.percentageDownloaded.ToString & "%")
+                                                        ProgressBar1.Invoke(Sub() ProgressBar1.Value = downloadStatusDetails.percentageDownloaded)
+                                                    End Sub
 
         ' Now we need to create our download thread.
         downloadThread = New Threading.Thread(Sub()
-                                                  Dim urlToFileToBeDownloaded As String = "https://www.toms-world.org/Install%20Restore%20Point%20Creator.exe"
-                                                  Dim pathToDownloadFileTo As String = "S:\Install Restore Point Creator.exe"
+                                                  Dim urlToFileToBeDownloaded As String = "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso"
+                                                  Dim pathToDownloadFileTo As String = "S:\ubuntu-16.04.1-desktop-amd64.iso"
                                                   Dim memStream As New IO.MemoryStream
 
                                                   Try
                                                       btnStopDownload.Enabled = True
                                                       btnDownloadFile.Enabled = False
+                                                      btnDownloadFile2.Enabled = False
 
                                                       ' We use the downloadFile() function which first calls for the URL and then the path to a place on the local file system to save it. This function is why we need multithreading, this will take a long time to do.
                                                       If httpHelper.downloadFile(urlToFileToBeDownloaded, memStream, True) = True Then
@@ -125,16 +126,19 @@ Public Class Form1
 
                                                   Catch ex As Net.WebException
                                                       btnDownloadFile.Enabled = True
+                                                      btnDownloadFile2.Enabled = True
                                                       btnStopDownload.Enabled = False
                                                       MsgBox(ex.Message & " " & ex.StackTrace)
                                                   Catch ex As Threading.ThreadAbortException
 
                                                       btnDownloadFile.Enabled = True
+                                                      btnDownloadFile2.Enabled = True
                                                       btnStopDownload.Enabled = False
                                                       If IO.File.Exists(pathToDownloadFileTo) Then IO.File.Delete(pathToDownloadFileTo)
                                                       MsgBox("Download aborted.") ' And tell the user that the download is aborted.
                                                   End Try
                                               End Sub)
+        downloadThread.IsBackground = True
         downloadThread.Start() ' Starts our download thread.
     End Sub
 
@@ -147,7 +151,7 @@ Public Class Form1
             If OpenFileDialog.ShowDialog() = DialogResult.OK Then
                 Dim strServerResponse As String = Nothing
 
-                Dim httpHelper As New Tom.httpHelper()
+                Dim httpHelper As New HTTPHelper.httpHelper()
                 httpHelper.setHTTPTimeout = 10
                 httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
                 httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/")
@@ -180,7 +184,7 @@ Public Class Form1
         Try
             Dim strServerResponse As String = Nothing
 
-            Dim httpHelper As New Tom.httpHelper()
+            Dim httpHelper As New HTTPHelper.httpHelper()
             httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
             httpHelper.addGETData("test3", "value3")
             httpHelper.addHTTPCookie("mycookie", "my cookie contents", "www.toms-world.org", "/")
@@ -195,12 +199,81 @@ Public Class Form1
                     TextBox1.Text &= certDetails.ToString
                 End If
             End If
-        Catch ex As Tom.httpProtocolException
+        Catch ex As HTTPHelper.httpProtocolException
             ' You can handle httpProtocolExceptions different than normal exceptions with this code.
         Catch ex As Net.WebException
             ' You can handle web exceptions different than normal exceptions with this code.
         Catch ex As Exception
             MsgBox(ex.Message & " " & ex.StackTrace)
         End Try
+    End Sub
+
+    Private Sub btnDownloadFile2_Click(sender As Object, e As EventArgs) Handles btnDownloadFile2.Click
+        ' First we create our httpHelper Class instance.
+        Dim httpHelper As New HTTPHelper.httpHelper()
+        httpHelper.setUserAgent = "Microsoft .NET" ' Set our User Agent String.
+
+        ' Now we need to create our download thread.
+        downloadThread = New Threading.Thread(Sub()
+                                                  Dim urlToFileToBeDownloaded As String = "http://releases.ubuntu.com/16.04.1/ubuntu-16.04.1-desktop-amd64.iso"
+                                                  Dim pathToDownloadFileTo As String = "S:\ubuntu-16.04.1-desktop-amd64.iso"
+                                                  Dim memStream As New IO.MemoryStream
+
+                                                  Try
+                                                      btnStopDownload.Enabled = True
+                                                      btnDownloadFile.Enabled = False
+                                                      btnDownloadFile2.Enabled = False
+
+                                                      ' We use the downloadFile() function which first calls for the URL and then the path to a place on the local file system to save it. This function is why we need multithreading, this will take a long time to do.
+                                                      If httpHelper.downloadFile(urlToFileToBeDownloaded, memStream, True) = True Then
+                                                          Dim fileStream As New IO.FileStream(pathToDownloadFileTo, IO.FileMode.Create)
+                                                          memStream.CopyTo(fileStream)
+                                                          memStream.Close()
+                                                          memStream.Dispose()
+                                                          fileStream.Close()
+                                                          fileStream.Dispose()
+
+                                                          btnDownloadFile.Enabled = True
+                                                          btnStopDownload.Enabled = False
+                                                          MsgBox("Download complete.") ' And tell the user that the download is complete.
+                                                      End If
+
+                                                  Catch ex As Net.WebException
+                                                      btnDownloadFile.Enabled = True
+                                                      btnDownloadFile2.Enabled = True
+                                                      btnStopDownload.Enabled = False
+                                                      MsgBox(ex.Message & " " & ex.StackTrace)
+                                                  Catch ex As Threading.ThreadAbortException
+
+                                                      btnDownloadFile.Enabled = True
+                                                      btnDownloadFile2.Enabled = True
+                                                      btnStopDownload.Enabled = False
+                                                      If IO.File.Exists(pathToDownloadFileTo) Then IO.File.Delete(pathToDownloadFileTo)
+                                                      MsgBox("Download aborted.") ' And tell the user that the download is aborted.
+                                                  End Try
+                                              End Sub)
+        downloadThread.IsBackground = True
+        downloadThread.Start() ' Starts our download thread.
+
+        statusThread = New Threading.Thread(Sub()
+                                                Dim oldFileSize As ULong = 0
+                                                Dim downloadStatusDetails As HTTPHelper.downloadStatusDetails
+startAgain:
+                                                downloadStatusDetails = httpHelper.getDownloadStatusDetails
+
+                                                If downloadStatusDetails IsNot Nothing Then
+                                                    Label1.Invoke(Sub() Label1.Text = String.Format("Downloaded {0} of {1} ({2}/s)", httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.remoteFileSize), httpHelper.fileSizeToHumanReadableFormat(downloadStatusDetails.localFileSize - oldFileSize)))
+
+                                                    oldFileSize = downloadStatusDetails.localFileSize
+
+                                                    Label2.Invoke(Sub() Label2.Text = downloadStatusDetails.percentageDownloaded.ToString & "%")
+                                                    ProgressBar1.Invoke(Sub() ProgressBar1.Value = downloadStatusDetails.percentageDownloaded)
+                                                End If
+
+                                                Threading.Thread.Sleep(1000)
+                                                GoTo startAgain
+                                            End Sub)
+        statusThread.IsBackground = True
+        statusThread.Start()
     End Sub
 End Class
