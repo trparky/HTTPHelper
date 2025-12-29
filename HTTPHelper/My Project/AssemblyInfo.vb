590c4e22dc554b6e567784bd034f73109baad9a1
Tom <Tom@TOMS-DESKTOP>
2025-12-28 23:46:29 -0500

    Updated the copyright line in the assembly information to include up to the year of 2030.

--- HTTPHelper/My Project/AssemblyInfo.vb
+++ HTTPHelper/My Project/AssemblyInfo.vb
@@ -2,7 +2,7 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
@@ -12,8 +12,8 @@ Imports System.Runtime.InteropServices
 <Assembly: AssemblyDescription("An HTTP Helper Library that allows you to do many HTTP-related stuff but without all of the hassle of writing your own code to do it.")>
 <Assembly: AssemblyCompany("")>
 <Assembly: AssemblyProduct("Tom's HTTPHelper (Requires Microsoft .NET 4.6)")>
-<Assembly: AssemblyCopyright("Copyright © Thomas R. Parkison 2023-2024")>
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCopyright("Copyright Thomas Parkison © 2023-2030")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
@@ -23,13 +23,13 @@ Imports System.Runtime.InteropServices
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("1.0.45.0")>
 <Assembly: AssemblyFileVersion("1.3.1.3")>
